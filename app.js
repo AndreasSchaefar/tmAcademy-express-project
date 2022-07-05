9d78157b9c14a9aaaa7163b0daaa57c57b97da16
AndreasSchaefar <skachko.andrii@gmail.com>
2022-07-05 20:11:33 +0300

    app-level json middleware

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from 'express';
 import { router as bookRouter } from './routes/api/books.js';
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
+app.use(express.json());
 app.use('/api/books', bookRouter);
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
